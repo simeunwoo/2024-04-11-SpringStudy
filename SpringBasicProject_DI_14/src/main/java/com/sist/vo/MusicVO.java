64eedf59e225c3c4475eeb0caefe91ed5b940e18
sist115_1 <sist115_1@sist115>
2024-08-29 09:18:06 +0900

    0829

---
+++ SpringBasicProject_DI_14/src/main/java/com/sist/vo/MusicVO.java
@@ -0,0 +1,15 @@
+package com.sist.vo;
+/*
+ MNO                                       NOT NULL NUMBER
+ TITLE                                     NOT NULL VARCHAR2(2000)
+ SINGER                                    NOT NULL VARCHAR2(2000)
+ ALBUM                                              VARCHAR2(2000)
+ POSTER                                    NOT NULL VARCHAR2(300)
+ STATE                                              CHAR(12)
+ IDCREMENT                                          NUMBER
+ */
+public class MusicVO {
+
+	private int mno,idcrement;
+	private String title,singer,album,state;
+}
