ffb29893d163174e25141db608c6d524d9624942
simeunwoo <simeunwoo11@naver.com>
2024-09-02 12:49:15 +0900

    0902

---
+++ SpringWebDIAOPProject_1/src/main/java/com/sist/dao/EmpDAO.java
@@ -0,0 +1,20 @@
+package com.sist.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import java.util.*;
+import com.sist.mapper.*;
+import com.sist.vo.*;
+
+@Repository // 메모리 할당
+public class EmpDAO {
+
+	// 구현된 클래스 읽기
+	@Autowired
+	private EmpMapper mapper;
+	
+	public List<EmpVO> empListData()
+	{
+		return mapper.empListData();
+	}
+}
