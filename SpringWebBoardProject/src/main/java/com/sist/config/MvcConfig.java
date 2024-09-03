f36b28476075cfdb167c6d3162fb9309fe0d1c25
simeunwoo <simeunwoo11@naver.com>
2024-09-03 16:52:31 +0900

    0903

--- SpringWebBoardProject/src/main/java/com/sist/config/MvcConfig.java
+++ SpringWebBoardProject/src/main/java/com/sist/config/MvcConfig.java
@@ -11,61 +11,75 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
+/*
+ *   <aop:aspectj-autoproxy/><!-- aop를 어노테이션으로 제작 -->
+     <context:component-scan base-package="com.sist.*"/>
+     <mybatis-spring:scan base-package="com.sist.mapper" factory-ref="ssf"/>
+ */
 @Configuration
-@ComponentScan(basePackages= {"com.sist.*"})
+// <context:component-scan base-package="com.sist.*"/>
+@ComponentScan(basePackages = {"com.sist.*"})
+// <aop:aspectj-autoproxy/>
 @EnableAspectJAutoProxy
-@MapperScan(basePackages= {"com.sist.mapper"})
-public class MvcConfig {
-	
+// <mybatis-spring:scan base-package="com.sist.mapper" factory-ref="ssf"/>
+@MapperScan(basePackages = {"com.sist.mapper"})
+public class MvcConfig implements WebMvcConfigurer{
+
 	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigur)
-	
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		// TODO Auto-generated method stub
+		configurer.enable();
+		// HandlerMapping을 메모리 할당 => Model안에서 RequestMapping , GetMapping , PostMapping
+	}
+	/*
+	 *   <bean id="viewResolver"
+          class="org.springframework.web.servlet.view.InternalResourceViewResolver"
+          p:prefix="/"
+          p:suffix=".jsp"
+       />
+	 */
 	@Bean("viewResolver")
-	public ViewResolver viewResolver() 
+	public ViewResolver viewResolver()
 	{
 		InternalResourceViewResolver wr=
 				new InternalResourceViewResolver();
 		wr.setPrefix("/");
 		wr.setSuffix(".jsp");
-		
 		return wr;
 	}
 	
 	/*
-	<bean id="ds" class="org.apache.commons.dbcp.BasicDataSource"
-		p:driverClassName="oracle.jdbc.driver.OracleDriver"
-		p:url="jdbc:oracle:thin:@localhost:1521:XE"
-		p:username="hr"
-		p:password="happy"
-	/>
+	 *   <bean id="ds" class="org.apache.commons.dbcp.BasicDataSource"
+		      p:driverClassName="oracle.jdbc.driver.OracleDriver"
+		      p:url="jdbc:oracle:thin:@localhost:1521:XE"
+		      p:username="hr"
+		      p:password="happy"
+		   />
+		   <bean id="ssf" class="org.mybatis.spring.SqlSessionFactoryBean"
+		      p:dataSource-ref="ds"
+		   />
 	 */
 	@Bean("ds")
 	public DataSource dataSource()
 	{
 		BasicDataSource ds=new BasicDataSource();
-		ds.setDriverClassName("oracle.jdbc.driver.oracleDriver");
+		ds.setDriverClassName("oracle.jdbc.driver.OracleDriver");
 		ds.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
 		ds.setUsername("hr");
 		ds.setPassword("happy");
-		
 		return ds;
 	}
 	
-	/*
-	<bean id="ssf" class="org.mybatis.spring.SqlSessionFactoryBean"
-		p:dataSource-ref="ds"
-	/>
-	 */
 	@Bean("ssf")
 	public SqlSessionFactory sqlSessionFactory() throws Exception
 	{
 		SqlSessionFactoryBean ssf=
 				new SqlSessionFactoryBean();
 		ssf.setDataSource(dataSource());
-		
 		return ssf.getObject();
 	}
-	
+   
 }
---
+++ SpringWebBoardProject/src/main/webapp/databoard/list.jsp
@@ -0,0 +1,111 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!--
+	웹 : 로직은 없다
+		흐름
+		요청 / 응답
+		 |     | 처리 결과값 => 화면에 출력 (JSP)
+		사용자가 전송
+		========== 어떤 값을 보내면 데이터베이스에서 찾기
+	
+	스프링
+		1) DI => 클래스 등록
+			필요 시에 멤버 변수의 초기화 : DI => (setXxx() ~> p:), (생성자 ~> c:)
+		2) AOP => 공통으로 적용되는 기능을 설정 => 자동으로 처리 => 인터셉터 (자동 로그인)
+			= log 파일 / 트랜잭션 / 보안 처리
+			= 어떤 메소드에 적용 (PointCut)
+			= 메소드 적용 위치 확인 (JoinPoint)
+			~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Advice
+			# 적용 위치
+				beford / after / after-returning / after-throwing
+				public void display()
+				{
+					=> 적용 : before
+					try
+					{
+					}catch(Exception ex)
+					{
+						=> 오류 처리 : after-throwing
+					}
+					finally
+					{
+						=> after
+					}
+					return => after-returning
+				}
+		3) MVC => 요청 => DispatcherServlet = HandlerMapping = Model = ViewResolver = View (JSP)
+			요청 => JSP = Model = JSP
+			            ~~~~~~
+			              |
+			              DB
+			=> 라이브러리 : 사용법 (보안)
+		 
+	
+	# AI
+	AI => 데이터 전처리 => 분석 => 통계 => 예측
+		데이터 전처리 : 데이터 수집 : before
+		예측 : after에서 주로 예측
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<style type="text/css">
+.container{
+	margin-top: 50px;
+}
+.row{
+	margin: 0px auto;
+	width: 800px;
+}
+</style>
+</head>
+<body>
+	<div class="container">
+		<h3 class="text-center">멀티 업로드 자료실</h3>
+		<div class="row">
+			<table class="table">
+				<tr>
+					<td>
+						<a href="insert.do" class="btn btn-sm btn-primary">등록</a>
+					</td>
+				</tr>
+			</table>
+			<table class="table table-hover">
+				<tr>
+					<th width="10%" class="text-center">번호</th>
+					<th width="45%" class="text-center">제목</th>
+					<th width="15%" class="text-center">이름</th>
+					<th width="20%" class="text-center">작성일</th>
+					<th width="10%" class="text-center">조회수</th>
+				</tr>
+				<c:set var="count" value="${count }"/>
+				<c:forEach var="vo" items="${list }">
+					<tr>
+						<td width="10%" class="text-center">${count }</td>
+						<td width="45%">
+							<a href="detail.do?no=${vo.no }">${vo.subject }</a>
+						</td>
+						<td width="15%" class="text-center">${vo.name }</td>
+						<td width="20%" class="text-center">${vo.dbday }</td>
+						<td width="10%" class="text-center">${vo.hit }</td>
+					</tr>
+					<c:set var="count" value="${count-1 }"/>
+				</c:forEach>
+			</table>
+			<table class="table">
+				<tr>
+					<td class="text-center">
+						<a href="#" class="btn btn-sm btn-danger">이전</a>
+							${curpage } page / ${totalpage } pages
+						<a href="#" class="btn btn-sm btn-danger">다음</a>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+</body>
+</html>
