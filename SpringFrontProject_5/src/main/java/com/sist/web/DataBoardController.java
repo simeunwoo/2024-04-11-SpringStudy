f8b494c30dbeda482e57d2e4fc3b64829b7416c8
simeunwoo <simeunwoo11@naver.com>
2024-09-23 15:51:09 +0900

    0923

--- SpringFrontProject_5/src/main/java/com/sist/web/DataBoardController.java
+++ SpringFrontProject_5/src/main/java/com/sist/web/DataBoardController.java
@@ -1,9 +1,21 @@
 package com.sist.web;
+import java.util.*;
+import com.sist.dao.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 // 화면만 변경
 @Controller
 public class DataBoardController {
 
+//	@Autowired
+//	private DataBoardDAO dao;
+	
+	@GetMapping("databoard/list.do")
+	public String databoard_list()
+	{
+		return "databoard/list";
+	}
 }
