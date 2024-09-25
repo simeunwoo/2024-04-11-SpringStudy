88e09d11ceb671738acc2bc385ad0564985ff7ad
simeunwoo <simeunwoo11@naver.com>
2024-09-25 09:51:45 +0900

    0925

--- SpringFrontProject_6/src/main/java/com/sist/web/FoodRestController.java
+++ SpringFrontProject_6/src/main/java/com/sist/web/FoodRestController.java
@@ -1,5 +1,55 @@
 package com.sist.web;
+import java.util.*;
+import com.sist.vo.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sist.dao.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FoodRestController {
 
+	// DAO => 스프링에서 메모리 할당 => getBean() => (X) => 스프링에서 주소값을 대입
+	@Autowired
+	private FoodDAO dao;
+	
+	@GetMapping(value="food/list_vue.do",produces="text/plain;charset=UTF-8")
+	/*
+	 * 	HTML : text/html (스크립트)
+	 * 	XML : text/xml
+	 * 	JSON (일반 문자열) : text/plain
+	 */
+	// JSON : 데이터를 모아서 자바스크립트로 전송할 목적
+	// => Ajax, Vue, React, Next ... Kotlin / Flutter
+	// 객체 : {키:값,키:값...} (VO) (JSON 방식)
+	// 목록 : [{},{}...] (List) (JSON 방식)
+	public String food_list(int page,String type) throws Exception
+	{
+		int rowSize=20;
+		int start=(rowSize*page)-(rowSize-1);
+		int end=rowSize*page;
+		
+		Map map=new HashMap();
+		map.put("start", start);
+		map.put("end", end);
+		map.put("type", type); // #{type} => map.get("type")
+		
+		List<FoodVO> list=dao.foodTypeListData(map);
+		int totalpage=dao.foodTypeTotalPage(type);
+		
+		map=new HashMap();
+		map.put("list", list); // model.addAttribute("list", list) => EL, JSTL
+		// map.put("list", list) => Vue에서 처리 => [] => food_list:[]
+		map.put("curpage", page); // => 정수 => curpage:1
+		map.put("totalpage", totalpage); // => 정수 => totalpage:0
+		map.put("type", type); // => 문자열 => type:'한식'
+		
+		// 자바스크립트에서 인식할 수 있도록 => JSON으로 변경
+		ObjectMapper mapper=new ObjectMapper();
+		String json=mapper.writeValueAsString(map);
+		
+		return json;
+	}
 }
--- SpringFrontProject_6/src/main/webapp/food/list.jsp
+++ SpringFrontProject_6/src/main/webapp/food/list.jsp
@@ -44,16 +44,65 @@ input[type="button"]{
 	<script>
 		let listApp=Vue.createApp({
 			data(){
+				// 초기값(초기화) 관련
 				return{
-					
+					food_list:[],
+					curpage:1,
+					totalpage:0,
+					type:'한식'
 				}
 			},
+			// 화면 출력 전에 변수의 초기화 => 서버 연결 => axios.get
+			// 반복 => 첫 화면, 페이지 (이전, 다음) => 함수로 제작 => 호출
 			mounted(){
-				
+				this.dataRecv()
 			},
 			methods:{
-				
+				// 서버 연결 반복 => 제거
+				// list_vue.do?page=1&type=한식
+				/*
+					$.ajax({
+						type:~, => get(), post(), put(), delete() 
+						url:~, => 함수 안에
+						data:~, => params
+						success:function(response)
+						{
+							then(function(response){})
+							then(response=>{})
+						},
+						error:function(request,status,error)
+						{
+							catch(error=>{})
+						}
+					})
+					
+					=> 요청/응답을 동시에 처리
+					=> 화면을 변경하지 않고 자체에서 처리 => 속도가 빠르다
+				*/
+				dataRecv(){
+					axios.get('list_vue.do',{
+						// Ajax => data:{page:1,type:'한식'}
+						params:{
+							page:this.curpage,
+							type:this.type
+						}
+					}).then(response=>{
+						console.log(response.data)
+						this.food_list=response.data.list
+						this.curpage=response.data.curpage
+						this.totalpage=response.data.totalpage
+						this.type=response.data.type
+					}).catch(error=>{
+						console.log(error.response)
+					})	
+				}
 			}
+			/*
+				1. 전체 코딩
+				2. 반복 =========== 공통 모듈 : AOP
+				3. 재사용 부분
+				   ======== 메소드화
+			*/
 		}).mount('.container')
 	</script>
 </body>
