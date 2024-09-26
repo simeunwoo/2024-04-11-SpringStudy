4c43ada943f1529470bb17d065cedffeeea3c7b8
simeunwoo <simeunwoo11@naver.com>
2024-09-26 11:09:55 +0900

    0926

--- SpringLastProject/src/main/java/com/sist/web/MainController.java
+++ SpringLastProject/src/main/java/com/sist/web/MainController.java
@@ -1,6 +1,7 @@
 package com.sist.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 /*
@@ -93,7 +94,7 @@ public class MainController {
 	// 필요한 클래스 => 스프링에서 가져 온다 (객체 주소)
 	// 사용자 요청에 따라 => 처리
 	@GetMapping("main.do")
-	public String main_main()
+	public String main_main(Model model)
 	{
 		return "main";
 	}
