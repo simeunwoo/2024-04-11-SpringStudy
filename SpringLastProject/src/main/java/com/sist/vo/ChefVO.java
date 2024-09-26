af915e785ce3c0bfd79e975e5eddc01c5f61c63d
simeunwoo <simeunwoo11@naver.com>
2024-09-26 10:33:00 +0900

    0926

---
+++ SpringLastProject/src/main/java/com/sist/vo/ChefVO.java
@@ -0,0 +1,15 @@
+package com.sist.vo;
+
+import lombok.Data;
+/*
+ CHEF                                      NOT NULL VARCHAR2(100)
+ POSTER                                             VARCHAR2(500)
+ MEM_CONT1                                          VARCHAR2(100)
+ MEM_CONT3                                          VARCHAR2(100)
+ MEM_CONT7                                          VARCHAR2(100)
+ MEM_CONT2                                          VARCHAR2(100)
+ */
+@Data
+public class ChefVO {
+
+}
