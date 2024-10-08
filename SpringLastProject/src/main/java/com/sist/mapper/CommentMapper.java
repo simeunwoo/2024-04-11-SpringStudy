e37da84b13f53ee36418e484fe3ebcdf91f6a4af
simeunwoo <simeunwoo11@naver.com>
2024-10-08 11:51:27 +0900

    1008

--- SpringLastProject/src/main/java/com/sist/mapper/CommentMapper.java
+++ SpringLastProject/src/main/java/com/sist/mapper/CommentMapper.java
@@ -10,7 +10,7 @@ public interface CommentMapper {
 	@Select("SELECT cno,rno,type,id,name,msg,likecount,TO_CHAR(regdate,'YYYY-MM-DD HH24:MI:SS') as dbday,num "
 			+ "FROM (SELECT cno,rno,type,id,name,msg,likecount,regdate,rownum as num "
 			+ "FROM (SELECT cno,rno,type,id,name,msg,likecount,regdate "
-			+ "FROM spring_comment WHERE rno=#{rno} "
+			+ "FROM spring_comment WHERE rno=#{rno} AND type=#{type} "
 			+ "ORDER BY group_id DESC,group_step ASC)) "
 			+ "WHERE num BETWEEN #{start} AND #{end}")
 	public List<CommentVO> commentListData(Map map);
--- SpringLastProject/src/main/java/com/sist/web/CommentRestController.java
+++ SpringLastProject/src/main/java/com/sist/web/CommentRestController.java
@@ -4,6 +4,7 @@ import com.sist.vo.*;
 import com.sist.service.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +12,27 @@ public class CommentRestController {
 
 	@Autowired
 	private CommentService cService;
+	
+	// 자주 호출이 필요하므로 만든 메소드
+	public String commonsListData(int page,int rno,int type)
+	{
+		Map map=new HashMap();
+		
+		int rowSize=10;
+		int start=(rowSize*page)-(rowSize-1);
+		int end=rowSize*page;
+		
+		map.put("start", start);
+		map.put("end", end);
+		map.put("rno", rno);
+		map.put("type", type);
+		
+		return "";
+	}
+	
+	@GetMapping(value="comment/list_vue.do",produces="text/plain;charset=UTF-8")
+	public String comment_list(int page,int rno,int type) throws Exception
+	{
+		return "";
+	}
 }
