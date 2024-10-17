c9fa6c1824bd1d04a2179d9a9181764f4837bc75
simeunwoo <simeunwoo11@naver.com>
2024-10-18 08:59:57 +0900

    1018

--- abcabc/src/main/java/com/sist/mapper/PlayerMapper.java
+++ abcabc/src/main/java/com/sist/mapper/PlayerMapper.java
@@ -1,10 +1,12 @@
 package com.sist.mapper;
 import java.util.*;
 
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.mapping.StatementType;
 
 import com.sist.vo.*;
 
@@ -26,14 +28,57 @@ public interface PlayerMapper {
 			+ "WHERE name LIKE '%'||#{fd}||'%'")
 	public int pitcherTotalPage(String fd);
 	
-	@Select("SELECT bno,age,game,h1,homerun,rbi,war,name,team,position,logo,image,avg,num "
+/*	@Select("SELECT bno,age,game,h1,homerun,rbi,war,name,team,position,logo,image,avg,num "
 			+ "FROM (SELECT bno,age,game,h1,homerun,rbi,war,name,team,position,logo,image,avg,rownum as num "
 			+ "FROM (SELECT bno,age,game,h1,homerun,rbi,war,name,team,position,logo,image,avg "
 			+ "FROM batter "
 			+ "WHERE name LIKE '%'||#{fd}||'%' "
 			+ "ORDER BY bno ASC)) "
 			+ "WHERE num BETWEEN #{start} AND #{end}")
+	public List<BatterVO> batterListData(Map map); */
+/*
+CREATE OR REPLACE PROCEDURE batterListData(
+    pFd IN VARCHAR2,
+    pStart IN NUMBER,
+    pEnd IN NUMBER,
+    pResult OUT SYS_REFCURSOR
+) AS
+BEGIN
+    OPEN pResult FOR
+    SELECT bno, age, game, h1, homerun, rbi, war, name, team, position, logo, image, avg, rownum as num
+    FROM (SELECT bno, age, game, h1, homerun, rbi, war, name, team, position, logo, image, avg
+          FROM batter
+          WHERE name LIKE '%' || pFd || '%'
+          ORDER BY bno ASC)
+    WHERE rownum BETWEEN pStart AND pEnd;
+END;
+/
+ */
+	
+	@Results({
+	    @Result(property="bno",column="bno"),
+	    @Result(property="age",column="age"),
+	    @Result(property="game",column="game"),
+	    @Result(property="h1",column="h1"),
+	    @Result(property="homerun",column="homerun"),
+	    @Result(property="rbi",column="rbi"),
+	    @Result(property="war",column="war"),
+	    @Result(property="name",column="name"),
+	    @Result(property="team",column="team"),
+	    @Result(property="position",column="position"),
+	    @Result(property="logo",column="logo"),
+	    @Result(property="image",column="image"),
+	    @Result(property="avg",column="avg"),
+	    @Result(property="num",column="num")
+	})
+	@Select(value="{CALL batterListData"
+			+ "(#{pFd,mode=INjavaType=java.lang.String},"
+			+ "#{pStart,mode=IN,javaType=java.lang.Integer},"
+			+ "#{pEnd,mode=IN,javaType=java.lang.Integer},"
+			+ "#{pResult,mode=OUT,jdbcType=CURSOR})}")
+	@Options(statementType = StatementType.CALLABLE)
 	public List<BatterVO> batterListData(Map map);
+
 	
 	@Select("SELECT pno,age,game,win,lose,save,hold,era,war,name,team,logo,image,num "
 			+ "FROM (SELECT pno,age,game,win,lose,save,hold,era,war,name,team,logo,image,rownum as num "
--- abcabc/src/main/java/com/sist/mapper/TeamMapper.java
+++ abcabc/src/main/java/com/sist/mapper/TeamMapper.java
@@ -1,7 +1,11 @@
 package com.sist.mapper;
 import java.util.*;
 
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.mapping.StatementType;
 
 import com.sist.vo.*;
 
@@ -12,10 +16,59 @@ public interface TeamMapper {
 			+ "ORDER BY name ASC")
 	public List<TeamDetailVO> teamListData();
 	
-	@Select("SELECT * FROM teamdetail "
-			+ "WHERE name=#{name}")
+//	@Select("SELECT * FROM teamdetail "
+//			+ "WHERE name=#{name}")
+//	public TeamDetailVO teamDetailData(String name);
+/*
+CREATE OR REPLACE PROCEDURE teamDetailData(
+    pName IN VARCHAR2,
+    pResult OUT SYS_REFCURSOR
+) AS
+BEGIN
+    OPEN pResult FOR
+    SELECT name, syear, content, winyear, home, dyear, oldteam, logo, rno, mascot, mimage, star
+    FROM teamdetail
+    WHERE name = pName;
+END;
+/
+ */
+	
+	@Results({
+		@Result(property="name",column="name"),
+		@Result(property="syear",column="syear"),
+		@Result(property="content",column="content"),
+		@Result(property="winyear",column="winyear"),
+		@Result(property="home",column="home"),
+		@Result(property="dyear",column="dyear"),
+		@Result(property="oldteam",column="oldteam"),
+		@Result(property="logo",column="logo"),
+		@Result(property="rno",column="rno"),
+		@Result(property="mascot",column="mascot"),
+		@Result(property="mimage",column="mimage"),
+		@Result(property="star",column="star")
+	})
+	@Select(value="{CALL teamRankingData"
+			+ "(#{pName,javaType=java.lang.String,mode=IN},"
+			+ "#{pResult,mode=OUT,jdbcType=CURSOR})}")
+	@Options(statementType=StatementType.CALLABLE)
 	public TeamDetailVO teamDetailData(String name);
 	
 	@Select("SELECT * FROM team ORDER BY no ASC")
 	public List<TeamVO> teamRankingData();
+/*
+@Results({
+	   @Result(property = "no",column = "no"),
+	   @Result(property = "title",column = "title"),
+	   @Result(property = "poster",column = "poster"),
+	   @Result(property = "address",column = "address"),
+	   @Result(property = "msg",column = "msg")
+   })
+   @Select(value="{CALL seoulShopDetailData(#{pNo,javaType=java.lang.Integer,mode=IN},"
+                +"#{pResult,mode=OUT,jdbcType=CURSOR,resultMap=seoulMap})}")
+   @Options(statementType = StatementType.CALLABLE)
+   public SeoulVO seoulShopDetailData(Map map);
+   
+   SELECT no,title,poster,address,msg
+		    FROM project_seoul_shop
+ */
 }
